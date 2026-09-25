b156c7aeed33cc9a1a9de23e45e2c650c7e93b3c
progress-ux <nikita15933@mail.ru>
2026-09-25 10:58:03 +0300

    add extern C for logger

--- src/util/logger.h
+++ src/util/logger.h
@@ -1,18 +1,26 @@
-#pragma once 
+#pragma once
 
-void LH_LogInit();
-void LH_LogShutdown();
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void LH_LogInit(void);
+void LH_LogShutdown(void);
 
 void LH_Log(const char* level, const char* format, ...);
 
-#define LH_INFO(...) LH_Log("INFO", __VA_ARGS__)
+#ifdef __cplusplus
+}
+#endif
+
+#define LH_INFO(...)  LH_Log("INFO", __VA_ARGS__)
 
 #ifdef LH_DEBUG_LOG
 #define LH_DEBUG(...) LH_Log("DEBUG", __VA_ARGS__)
 #else
 #define LH_DEBUG(...) ((void)0)
-#endif // LH_DEBUG_LOG
+#endif
 
-#define LH_WARN(...) LH_Log("WARN", __VA_ARGS__)
+#define LH_WARN(...)  LH_Log("WARN", __VA_ARGS__)
 #define LH_ERROR(...) LH_Log("ERROR", __VA_ARGS__)
 #define LH_START(...) LH_Log("START", __VA_ARGS__)
